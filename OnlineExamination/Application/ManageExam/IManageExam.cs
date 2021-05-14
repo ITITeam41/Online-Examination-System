8b8138f8496b3d4915e1fef015dc185330224cdf
HossamAmer <hossam.amer666@gmail.com>
2021-05-14 17:19:47 +0200

    ManageExam

---
+++ OnlineExamination/Application/ManageExam/IManageExam.cs
@@ -0,0 +1,18 @@
+﻿using OnlineExamination.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineExamination.Application.ManageExam
+{
+    public interface IManageExam
+    {
+        List<Exam> GetAllQuestions();
+        Exam GetQuestionById(int questionId);
+        List<Exam> GetQuestionByName(string searchName);
+        void EditQuestion(Exam exam);
+        void deleteQuestion(int questionId);
+        void AddNewQuestion(Exam exam);
+    }
+}
---
+++ OnlineExamination/Application/ManageExam/ManageExam.cs
@@ -0,0 +1,41 @@
+﻿using OnlineExamination.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineExamination.Application.ManageExam
+{
+    public class ManageExam : IManageExam
+    {
+        public void AddNewQuestion(Exam exam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteQuestion(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditQuestion(Exam exam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Exam> GetAllQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Exam GetQuestionById(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Exam> GetQuestionByName(string searchName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
